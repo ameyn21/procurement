b65c98f8faacd1f187f0138081b4996d5a2659dc
ameyn21 <ameyn21@gmail.com>
2018-11-08 19:16:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if($method == "POST") {
 	}
 	
 	$response = new \stdClass();
-	$response->fulfillmentText = $speech;
+	$response->speech = $speech;
 	$response->displayText = $speech;
 	$response->source = "webhook";
 	echo json_encode($response);
