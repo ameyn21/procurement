9183415352aadcc8bea9a36cc032d8e1b42a32df
ameyn21 <ameyn21@gmail.com>
2018-11-08 18:49:43 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method == "POST") {
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	
+	$text = $json-> queryResult->intent->displayName;
+	
+	switch($text) {
+		case 'Procure':
+			$speech = "PHP: We will buy it soon";
+			break;
+		default:
+			$speech = "PHP:Sorry, I didnt get that. Please ask me something else.";
+			break;		
+	}
+	
+	$response = new \stdClass();
+	$response->fulfillmentText = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";	
+}
+
+?>
