067921519d08af2a806d3f5073884333e8d05d2e
ameyn21 <ameyn21@gmail.com>
2018-11-09 12:19:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 use Dialogflow\WebhookClient;	
 //$agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
 $method = $_SERVER['REQUEST_METHOD'];
+$servername = "db.dpoplus.ai";
+$username = "root";
+$password = "alskdjfhg";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 
 if($method == "POST") {
 	$requestBody = file_get_contents('php://input');
